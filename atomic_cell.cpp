c292ff039bf9822d667843d9603e9f8bb7f757f5
Joachim LF <joachimlf@pm.me>
2021-02-23 10:06:23 +0100

    Changed selection display

--- atomic_cell.cpp
+++ atomic_cell.cpp
@@ -29,16 +29,16 @@ KDColor AtomicCell::colorForType(AtomType type) const {
 
 
 void AtomicCell::drawRect(KDContext * ctx, KDRect rect) const {
-  KDColor highlightColor = KDColor::RGB24(0x313431);
   if(m_visible) {
     KDColor color = colorForType(m_atom.type);
     ctx->fillRect(rect, color);
     KDPoint textPosition(bounds().topLeft().x() + 2, bounds().topLeft().y() + 2);
     ctx->drawString(m_atom.symbol, textPosition, KDFont::SmallFont, KDColorBlack, color);
     if(isHighlighted()) {
-      ctx->strokeRect(bounds(), KDColorBlack);
+      KDColor highlighColor = KDColor::blend(color, KDColorBlack, 0x7F);
+      ctx->strokeRect(bounds(), highlighColor);
       KDRect rect2(bounds().topLeft().x() + 1, bounds().topLeft().y() + 1, bounds().width() - 2, bounds().height() - 2);
-      ctx->strokeRect(rect2, KDColorBlack);
+      ctx->strokeRect(rect2, highlighColor);
     }
   } else {
     ctx->fillRect(rect, KDColorWhite);
